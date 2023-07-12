170e0bd27f05d9f172f93e09014d52525c895bca
jhoffman-ms <jmh248@gmail.com>
2023-07-12 11:12:34 -0300

    feat: add logger for the request

--- Doppler.CloverAPI/Controllers/PaymentController.cs
+++ Doppler.CloverAPI/Controllers/PaymentController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using Doppler.CloverAPI.DopplerSecurity;
 using Doppler.CloverAPI.Exceptions;
@@ -6,17 +7,21 @@ using Doppler.CloverAPI.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 
 namespace Doppler.CloverAPI.Controllers
 {
-    public class PaymentController : Controller
+    public partial class PaymentController : Controller
     {
         private readonly ICloverService _cloverService;
         private readonly string _paymentType = "";
+        private readonly ILogger<PaymentController> _logger;
 
-        public PaymentController(ICloverService cloverService)
+        public PaymentController(ICloverService cloverService, ILogger<PaymentController> logger)
         {
             _cloverService = cloverService;
+            _logger = logger;
         }
 
         [Authorize(Policies.OwnResourceOrSuperuser)]
@@ -25,6 +30,8 @@ namespace Doppler.CloverAPI.Controllers
         {
             try
             {
+                LogInformation($"Json request: {JsonConvert.SerializeObject(paymentRequest)}");
+
                 var paymentResponse = await _cloverService.CreatePaymentAsync(_paymentType, paymentRequest.ChargeTotal, paymentRequest.CreditCard, paymentRequest.ClientId, accountname);
                 return Ok(paymentResponse);
             }
@@ -38,6 +45,8 @@ namespace Doppler.CloverAPI.Controllers
         [HttpPost("/accounts/{accountname}/refund")]
         public async Task<IActionResult> CreateRefund([FromRoute] string accountname, [FromBody] RefundRequest refundRequest)
         {
+            LogInformation($"Json request: {JsonConvert.SerializeObject(refundRequest)}");
+
             var refundResponse = await _cloverService.CreateRefundAsync(refundRequest.ChargeTotal, refundRequest.ChargeAuthorizationNumber, accountname);
             return Ok(refundResponse);
         }
@@ -48,6 +57,8 @@ namespace Doppler.CloverAPI.Controllers
         {
             try
             {
+                LogInformation($"Json request: {JsonConvert.SerializeObject(creditCardRequest)}");
+
                 var isValidResponse = await _cloverService.IsValidCreditCard(creditCardRequest.CreditCard, creditCardRequest.ClientId, accountname);
                 return Ok(isValidResponse);
             }
@@ -56,5 +67,8 @@ namespace Doppler.CloverAPI.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.ApiError);
             }
         }
+
+        [LoggerMessage(0, LogLevel.Information, "{message}")]
+        partial void LogInformation(string message);
     }
 }
